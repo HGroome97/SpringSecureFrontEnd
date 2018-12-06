34f6bdfa1e490c53c0e8015f1b08761109442752
WareJosephB <ware.josephb@gmail.com>
2018-12-06 10:33:57 +0000

    mySql and admin priviledges

--- src/main/java/com/javacodegeeks/SecurityConfig.java
+++ src/main/java/com/javacodegeeks/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.javacodegeeks;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,10 +14,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+	@Autowired
+	DataSource dataSource;
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-        		.antMatchers("/admin").hasRole("ADMIN")
+        		.antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
         		.antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
             .and()
@@ -26,11 +32,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             	.logout()
                 .permitAll();
     }
-
+    
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("javacodegeeks").password("very_secure").roles("USER");
-        auth.inMemoryAuthentication().withUser("tom").password("toby").roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-    }
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+      auth.jdbcAuthentication().dataSource(dataSource)
+     .usersByUsernameQuery("select username,password, enabled from users where username=?")
+     .authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+    } 
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("javacodegeeks").password("very_secure").roles("USER");
+//        auth.inMemoryAuthentication().withUser("tom").password("toby").roles("USER");
+//        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+//    }
 }
--- src/main/java/com/javacodegeeks/SpringConfig.java
+++ src/main/java/com/javacodegeeks/SpringConfig.java
@@ -1,6 +1,8 @@
 package com.javacodegeeks;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -14,5 +16,15 @@ public class SpringConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/mydetails").setViewName("mydetails");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/admin").setViewName("admin");
+    }
+    
+    @Bean(name = "dataSource")
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/userbd");
+        driverManagerDataSource.setUsername("root");
+        driverManagerDataSource.setPassword("password");
+        return driverManagerDataSource;
     }
 }
